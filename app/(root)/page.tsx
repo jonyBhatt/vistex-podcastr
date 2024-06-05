25c8c6f2f68bf6fdbea8a3ea164e56400000a596
Jony <jonybhattacharjee886@gmail.com>
2024-06-05 23:59:22 +0600

    style: :sparkles: HomePage

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,22 @@
+import PodcastCard from "@/components/PodcastCard";
+import { podcastData } from "@/constants";
+
 function Home() {
   return (
-    <div className="mt-9 flex flex-col gap-9">
+    <div className="mt-9 flex flex-col gap-9 font-manrope">
       <section className="flex  flex-col gap-5">
-        <h1 className="text-20 font-bold text-white-1">Trending Podcast</h1>
+        <h1 className="text-20 font-bold text-white-1 ">Trending Podcast</h1>
+        <div className="podcast_grid">
+          {podcastData.map((podcast) => (
+            <PodcastCard
+              key={podcast.id}
+              id={podcast.id}
+              description={podcast.description}
+              imgURL={podcast.imgURL}
+              title={podcast.title}
+            />
+          ))}
+        </div>
       </section>
     </div>
   );
