b138c3edce494c74a0d5428570215b562232f689
Jony <jonybhattacharjee886@gmail.com>
2024-06-13 21:42:08 +0600

    update issues

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,7 @@
-"use client";
-import { useQuery } from "convex/react";
-import { api } from "@/convex/_generated/api";
 import PodcastCard from "@/components/PodcastCard";
 import { podcastData } from "@/constants";
 
 function Home() {
-  const tasks = useQuery(api.tasks.get);
   return (
     <div className="mt-9 flex flex-col gap-9 font-manrope">
       <section className="flex  flex-col gap-5">
